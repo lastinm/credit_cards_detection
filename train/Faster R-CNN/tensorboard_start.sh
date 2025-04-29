8b8b02b176e3583d2f214929ca28f9c6fe251e84
Maxim Lastin <lastinm@mail.ru>
2025-04-29 23:07:54 +0300

    Added chapter about metrics FasterRCNN to doc

--- train/Faster R-CNN/tensorboard_start.sh
+++ train/Faster R-CNN/tensorboard_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tensorboard --logdir '/home/lastinm/PROJECTS/CV/experiments/FasterRCNN-Torchvision-FineTuning-v2/exp/summary/09-04-2025-06-15-03' --port 6006
+tensorboard --logdir '/home/lastinm/PROJECTS/credit_cards_detection/train/Faster R-CNN/exp/summary/09-04-2025-06-15-03' --port 6006
---
+++ train/YOLOv12/tensorboard_start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+tensorboard --logdir 'file:///home/lastinm/PROJECTS/credit_cards_detection/train/YOLOv12/runs/detect/train 8n 150ep middle size color' --port 6006
